18e91813c73f78ab2b0fcd47985788b89f390cf2
Bogdan405 <Bogdan405@outlook.com>
2020-05-10 20:44:49 +0300

    Added new variant for app

---
+++ 3 - App Var 08-05-2020/README.txt
@@ -0,0 +1,2 @@
+This directory is dedicated only to the final project.
+The design pattern that will be used is MVC.
---
+++ 3 - App Var 08-05-2020/Server/fiimentor/controllers/index.ts
@@ -0,0 +1,6 @@
+import {getAllUsers} from './userController'
+import {createPost, getPostByPostId,getPostList,getPrivateMessageByPrivateMessageId} from './postController'
+import {tutorGroupeList} from "./tutorGroupeController";
+import {getProfessorList} from './professorController'
+import {getGroupeList} from "./groupeController";
+export {getAllUsers, createPost,  getGroupeList,getProfessorList,tutorGroupeList,getPostByPostId,getPostList,getPrivateMessageByPrivateMessageId}
