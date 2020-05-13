6503ab90c9673a95fd0446861db29ae85ada8651
Codrin Donciu <33196995+Codrinator@users.noreply.github.com>
2020-05-13 18:53:02 +0300

    Update

---
+++ Codrin/Lab11/post (realistic QA update).sql
@@ -0,0 +1,56 @@
+insert into post (Id, UserId, GroupeId, Title, Content) values (1, 901, 1, 'Constrângerile aplicabile licenței', 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (2, 902, 2, 'Adeverință', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (3, 903, 3, 'Profesori disponibili (update)', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (4, 904, 4, 'Stagii pe bune', 'Fusce consequat. Nulla nisl. Nunc nisl.
+
+Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (5, 905, 5, 'Licență', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (6, 906, 6, 'Licență', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
+
+Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (7, 907, 7, 'Profesori disponibili (update)', 'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (8, 908, 8, 'Alternativa temei de licență', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (9, 909, 9, 'Hai cu noi în America', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (10, 910, 10, 'Profesori disponibili (update)', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (11, 911, 11, 'Cerere voluntariat', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (12, 912, 12, 'Profesori disponibili (update)', 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (13, 913, 13, 'Alegerea domeniului', 'Fusce consequat. Nulla nisl. Nunc nisl.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (14, 914, 14, 'ASCII te vrea alături', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (15, 915, 15, 'Solicitare', 'Fusce consequat. Nulla nisl. Nunc nisl.
+
+Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (16, 916, 16, 'Cerere voluntariat', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (17, 917, 17, 'Aplicație Software (new version)', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.
+
+Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (18, 918, 18, 'Modificări licență', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (19, 919, 19, 'Orientare profesională', 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.
+
+Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (20, 920, 20, 'Adeverință', 'In congue. Etiam justo. Etiam pretium iaculis justo.
+
+In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (21, 921, 21, 'Solicitare', 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (22, 922, 22, 'Update lucrare de licență', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (23, 923, 23, 'Adeverință', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.
+
+Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (24, 924, 24, 'Alegerea domeniului', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (25, 925, 25, 'Constrângerile aplicabile licenței', 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (26, 926, 26, 'Orientare profesională', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (27, 927, 27, 'Modificări licență', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
+
+Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (28, 928, 28, 'Modificări licență', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (29, 929, 29, 'ASCII te vrea alături', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.');
+insert into post (Id, UserId, GroupeId, Title, Content) values (30, 930, 30, 'Adeverință', 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.');
